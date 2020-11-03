38d60a46576db5a5c21989dbd48baced4fd175b9
SacredRider02 <73513022+SacredRider02@users.noreply.github.com>
2020-11-03 14:47:12 +0600

    Update main.py

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ from aiogram import Bot, Dispatcher, executor
 
 loop = asyncio.get_event_loop()
 bot = Bot(BOT_TOKEN, parse_mode='HTML')
-dp = Dispatcher(bot, loop = loop)
+dp = Dispatcher(bot, loop=loop)
 
 if __name__ == "__main__":
-    from handlers import dp,send_to_admin
-    executor.start_polling(dp,on_startup=send_to_admin)
+    from handlers import dp, send_to_admin
+    executor.start_polling(dp, on_startup=send_to_admin)
